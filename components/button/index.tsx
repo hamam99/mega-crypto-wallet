1e34a6b85bf1ec7e7b525b457192ca788bb0aa63
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-06-20 15:16:11 +0700

    create button component

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,36 +1,60 @@
-import {
-  StyleProp,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  ViewStyle,
-} from "react-native";
+import classNames from "classnames";
+import { Text, TouchableOpacity } from "react-native";
 
+type IMode = "primary" | "secondary";
 type ButtonProps = {
-  label: string;
-  onPress: () => void;
-  mode: "primary" | "secondary";
-  containerStyle?: StyleProp<ViewStyle> | undefined;
-  labelStyle?: StyleProp<ViewStyle> | undefined;
+  label?: string;
+  onPress?: () => void;
+  mode?: IMode;
+  disabled?: boolean | undefined;
+  containerClassname?: string;
+  labelClassname?: string;
 };
 
 const Button = ({
   label,
   onPress = () => {},
-  containerStyle,
-  labelStyle,
+  containerClassname,
+  labelClassname,
+  mode = "primary",
 }: ButtonProps) => {
   return (
-    <TouchableOpacity onPress={onPress} style={containerStyle}>
-      <Text style={labelStyle}> {label}</Text>
+    <TouchableOpacity
+      onPress={onPress}
+      className={classNames(
+        "rounded-lg w-full h-[56px] justify-center items-center",
+        getContainerTheme(mode),
+        containerClassname,
+      )}
+    >
+      <Text
+        className={classNames(
+          "text-base font-bold",
+          getLabelTheme(mode),
+          labelClassname,
+        )}
+      >
+        {label}
+      </Text>
     </TouchableOpacity>
   );
 };
 export default Button;
 
-const styles = StyleSheet.create({
-  container: {
-    
-  },
-  label: {},
-});
+const getContainerTheme = (mode: IMode = "primary") => {
+  switch (mode) {
+    case "secondary":
+      return classNames(`bg-slate-300`);
+    default:
+      return classNames(`bg-black`);
+  }
+};
+
+const getLabelTheme = (mode: IMode = "primary") => {
+  switch (mode) {
+    case "secondary":
+      return classNames(`text-black]`);
+    default:
+      return classNames("text-yellow-300");
+  }
+};
