e01e92addee444497a1a708631031556bc07e3ed
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-06-21 16:12:34 +0700

    input password

---
+++ components/input-password/index.tsx
@@ -0,0 +1,55 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+
+type InputPasswordProps = {
+  value?: string;
+  onChangeText?: (text: string) => void;
+  secureTextEntry?: boolean;
+  label?: string;
+  error?: string;
+};
+
+const InputPassword = ({
+  value,
+  onChangeText = () => {},
+  secureTextEntry = false,
+  label,
+  error,
+}: InputPasswordProps) => {
+  const [secureTextEntryLocal, setSecureTextEntryLocal] = useState(false);
+
+  useEffect(() => {
+    setSecureTextEntryLocal(secureTextEntry);
+  }, [secureTextEntry]);
+  return (
+    <View className="">
+      <View className="w-full rounded-[4px] py-2 px-4 bg-white">
+        <Text className="text-slate-400">{label}</Text>
+        <View className="flex-row items-center">
+          <TextInput
+            value={value}
+            secureTextEntry={secureTextEntryLocal}
+            onChangeText={onChangeText}
+            className="flex-1 h-[40px]"
+          />
+          <TouchableOpacity
+            onPress={() => {
+              setSecureTextEntryLocal(!secureTextEntryLocal);
+            }}
+          >
+            <Entypo
+              name={secureTextEntryLocal ? "eye-with-line" : "eye"}
+              size={24}
+              color="black"
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {error && <Text className="text-red-500 text-xs px-3 py-1">{error}</Text>}
+    </View>
+  );
+};
+
+export default InputPassword;
