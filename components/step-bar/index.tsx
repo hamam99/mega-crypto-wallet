46c97e19d4ee3f948b2634325e2438051e8e499d
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-07-04 09:39:11 +0700

    update

--- components/step-bar/index.tsx
+++ components/step-bar/index.tsx
@@ -1,18 +1,36 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import classNames from "classnames";
+import { useRouter } from "expo-router";
 
 type StepbarProps = {
   currentStep?: number;
   totalSteps?: number;
+  onClose?: () => void;
 };
 
-const StepBar = ({ currentStep = 1, totalSteps = 1 }: StepbarProps) => {
+const StepBar = ({
+  currentStep = 1,
+  totalSteps = 1,
+  onClose,
+}: StepbarProps) => {
   const percentage = ((currentStep / totalSteps) * 100)?.toFixed() + "%";
 
+  const router = useRouter();
   return (
     <View className="w-full h-[44px] flex-row items-center gap-x-3">
-      <Ionicons name="close" size={24} color="black" />
+      <TouchableOpacity
+        onPress={() => {
+          if (onClose) {
+            onClose();
+            return;
+          }
+
+          router.back();
+        }}
+      >
+        <Ionicons name="close" size={24} color="black" />
+      </TouchableOpacity>
       <View className="flex-1 max-w-fit">
         <View
           className={classNames("h-2 bg-black rounded-[100px]")}
